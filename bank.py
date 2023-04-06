750c2c61ca4e6e0b00bd928cc759997f838faa92
Adedoyinsola Fifo <G00426046@atu.ie>
2023-04-06 22:49:11 +0100

    updating

--- bank.py
+++ bank.py
@@ -1,6 +1,8 @@
 # bank.py
 # Author : Adedoyinsola Fifo
+## Requesting input from user
+Amount1 = int(input('Enter Amount1 in cent: '))
+Amount2 = int(input('Enter Amount2 in cent: '))
 
-ans =(0.50+0.20)
-
-print("The sum of these is" + str(ans)
+Total_Amount = (Amount1 + Amount2)
+print (f"The Sum of these is: \u20AC{Total_Amount:.2f}")
