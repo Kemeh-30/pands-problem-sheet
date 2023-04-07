3e0a98876c964bb72b5c0ef1cd27d6b63547c3ec
Adedoyinsola Fifo <G00426046@atu.ie>
2023-04-07 23:17:10 +0100

    week 3

---
+++ accounts.py
@@ -0,0 +1,8 @@
+# accounts.py
+# Author : Adedoyinsola Fifo
+# Account number
+account_number = input("1234567890: ")
+last_four_digits= account_number[-4:]
+
+print("Account Number: ****" + last_four_digits)
+
