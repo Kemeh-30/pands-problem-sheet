bb717642e597c856e522cee12329cf0c5a926ac1
Kemeh-30 <127454113+Kemeh-30@users.noreply.github.com>
2023-05-14 21:41:29 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,8 +1,11 @@
 # bank.py
 # Author : Adedoyinsola Fifo
-## Requesting input from user
+# Prompt the user and read in two money amounts (in cent)
 Amount1 = int(input('Enter Amount1 in cent: '))
 Amount2 = int(input('Enter Amount2 in cent: '))
-
+# Add the two amounts
 Total_Amount = (Amount1 + Amount2)
+# Print out the answer in a human readable format with a euro sign and decimal point
 print (f"The Sum of these is: \u20AC{Total_Amount:.2f}")
+
+
