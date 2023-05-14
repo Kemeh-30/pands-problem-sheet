3697b1f7ef1a96eace366c4e8722326c21e3b4a9
Kemeh-30 <127454113+Kemeh-30@users.noreply.github.com>
2023-05-14 21:50:28 +0100

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -1,7 +1,9 @@
 # accounts.py
 # Author : Adedoyinsola Fifo
 # Account number display
+# Read in a 10 character account number
 account_number = input("1234567890: ")
 last_four_digits= account_number[-4:]
-
+# Output the account number with only the last 4 digits showing and the first 6 digits replaced with Xs
 print("Account Number: ****" + last_four_digits)
+
