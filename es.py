cc10c52fd2c9cc1ad50ef220815574026c380bd8
Kemeh-30 <127454113+Kemeh-30@users.noreply.github.com>
2023-05-14 21:55:59 +0100

    Update es.py

--- es.py
+++ es.py
@@ -1,9 +1,9 @@
 import sys
-
+# To Check if a filename argument was provided
 if len(sys.argv) < 2:
     print("Error: Please provide the filename as an argument.")
     sys.exit(1)
-
+# Read in the text file and count the number of e's
 filename = sys.argv[1]
 try:
     with open(filename, 'r') as file:
@@ -16,5 +16,7 @@ count = 0
 for char in text:
     if char.lower() == 'e':
         count += 1
-
+# Output the number of e's in the text file
 print(f"The file '{filename}' contains {count} occurrences of the letter 'e'.")
+
+
